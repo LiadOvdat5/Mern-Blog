687f9baa5e4369e4314f78d80eb8dcbe3e2e676e
LiadOvdat5 <LiadOvdat5@gmail.com>
2024-02-26 20:38:20 +0200

    add middleware function to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,30 +1,36 @@
-import User from '../models/user.model.js';
-import bcryptjs from 'bcryptjs';
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
-    const { username, email, password } = req.body;
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
 
-    // Validate the request
-    if (!username || !email || !password ||
-         username === '' || email === '' || password === '') {
-        return res.status(400).json({ message: 'All fields are required' });
-    }
+  // Validate the request
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    next(errorHandler(400, "All input is required"));
+  }
 
-    // Hash the password
-    const hashedPassword = bcryptjs.hashSync(password, 10);
+  // Hash the password
+  const hashedPassword = bcryptjs.hashSync(password, 10);
 
-    // Create a new user
-    const newUser = new User({ 
-        username, 
-        email, 
-        password: hashedPassword 
-    });
+  // Create a new user
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
 
-    try {
-        await newUser.save();
-        res.json('User created' );
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-    
+  try {
+    await newUser.save();
+    res.json("User created");
+  } catch (err) {
+    next(err);
+  }
 };
--- api/index.js
+++ api/index.js
@@ -1,17 +1,20 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
 // Connect to MongoDB
 mongoose
-    .connect(process.env.MONGO)
-    .then( () => { console.log('Connected to MongoDB') 
-    }).catch( (err) => { console.log('Error:', err.message);
-});
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error:", err.message);
+  });
 
 // Initialize express
 const app = express();
@@ -20,12 +23,20 @@ const app = express();
 app.use(express.json());
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-} );
-
+  console.log("Server is running on port 3000");
+});
 
 // Routes
-app.use('/api/user', userRoutes); //test api route
-app.use('/api/auth', authRoutes);
+app.use("/api/user", userRoutes); //test api route
+app.use("/api/auth", authRoutes);
 
-
+// Middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
