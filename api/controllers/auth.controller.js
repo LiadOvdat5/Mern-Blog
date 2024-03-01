4f03298e16aaaabf96c9ea6073960b3ac3a3b045
LiadOvdat5 <LiadOvdat5@gmail.com>
2024-03-01 14:22:49 +0200

    Add signin functionality (API route)

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -30,6 +31,44 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.json("User created");
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate the request
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All fields is required"));
+  }
+
+  try {
+    // Find the user
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(400, "Invalid password or email"));
+    }
+
+    // Compare the password
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid password or email"));
+    }
+
+    // Create a token
+    const token = jwt.sign({ id: validUser._id }, process.env.TOKEN_SECRET);
+
+    const { password: pass, ...rest } = validUser._doc;
+
+    // Send the token
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
   } catch (err) {
     next(err);
   }
