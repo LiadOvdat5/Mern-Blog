166ea7b85ae3d6d528f3eb0d06da39a7a3409a81
LiadOvdat5 <LiadOvdat5@gmail.com>
2024-03-02 16:01:19 +0200

    Add avatar dropdown menu to header component

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -1,10 +1,34 @@
-import { Button, Navbar, TextInput } from "flowbite-react";
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa6";
+import { useSelector } from "react-redux";
 
 function Header() {
   const path = useLocation().pathname;
+  const { currentUser } = useSelector((state) => state.user);
+
+  function AvatarDropDown() {
+    return (
+      <Dropdown
+        arrowIcon={false}
+        inline
+        label={<Avatar alt="user" img={currentUser.profilePicture} rounded />}
+      >
+        <Dropdown.Header>
+          <span className="block text-sm">@{currentUser.username}</span>
+          <span className="block text-sm font-medium truncate">
+            {currentUser.email}
+          </span>
+        </Dropdown.Header>
+        <Link to="/dashboard?tab=profile">
+          <Dropdown.Item>Profile</Dropdown.Item>
+        </Link>
+        <Dropdown.Divider />
+        <Dropdown.Item>Sign out</Dropdown.Item>
+      </Dropdown>
+    );
+  }
 
   return (
     <Navbar className="border-b-2">
@@ -38,9 +62,13 @@ function Header() {
           <FaMoon />
         </Button>
         <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue" outline>
-            Sign In
-          </Button>
+          {currentUser ? (
+            AvatarDropDown()
+          ) : (
+            <Button gradientDuoTone="purpleToBlue" outline>
+              Sign Out
+            </Button>
+          )}
         </Link>
         <Navbar.Toggle />
       </div>
