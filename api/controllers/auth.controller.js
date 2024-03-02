71cba623d8dd871f8c1914018ed3440f9f62cd74
LiadOvdat5 <LiadOvdat5@gmail.com>
2024-03-02 13:22:55 +0200

    Add Firebase and OAuth functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -73,3 +73,50 @@ export const signin = async (req, res, next) => {
     next(err);
   }
 };
+
+// Handle Google sign-in
+export const google = async (req, res, next) => {
+  const { email, name, googlePhotoUrl } = req.body;
+  try {
+    // Check if user already exists
+    const user = await User.findOne({ email });
+    if (user) {
+      // User exists, generate token
+      const token = jwt.sign({ id: user._id }, process.env.TOKEN_SECRET);
+      const { password, ...rest } = user._doc;
+      res
+        .status(200)
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .json(rest);
+    } else {
+      // User does not exist, create new user
+      // Generate password
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+      const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+      const newUser = new User({
+        username:
+          name.toLowerCase().split(" ").join("") +
+          Math.random().toString(9).slice(-4),
+        email,
+        password: hashedPassword,
+        profilePicture: googlePhotoUrl,
+      });
+      await newUser.save();
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const { password, ...rest } = newUser._doc;
+      res
+        .status(200)
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .json(rest);
+    }
+  } catch (error) {
+    next(error);
+    console.log(error);
+  }
+};
